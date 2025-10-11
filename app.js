4221a873881eca59323d406f2d863d036bf149cc
vyom Priyesh <vyompriyesh@gmail.com>
2025-10-11 13:18:51 +0530

    main

--- app.js
+++ app.js
@@ -1,35 +1,32 @@
 import express from "express";
-import cors from 'cors'
+import cors from "cors";
 import connectDB from "./db/Connectdb.js";
 import api from "./routes/Api.js";
-import { assetsUrl, folderName } from "./Config.js";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
-const port = process.env.PORT
-const DATABASE_URL = process.env.DATABASE_URL
+
+const port = process.env.PORT || 3000;
+const DATABASE_URL = process.env.DATABASE_URL;
 
 connectDB(DATABASE_URL);
 
 app.use(express.json());
 app.use(cors());
 
+const assetsUrl = path.join(__dirname, "assets");
+app.use("/assets", express.static(assetsUrl));
+
+app.get("/", (req, res) => res.send("Hello from Express on Vercel!"));
+app.use("/api", api);
+
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).json({
-    success: false,
-    error: "Internal server error",
-  });
+  res.status(500).json({ success: false, error: "Internal server error" });
 });
-app.get('/', (req, res) => res.send('Hello from Express on Vercel!'));
-app.use(`/${folderName}`, express.static(assetsUrl));
-// app.use(express.static(path.resolve(assetsUrl, './', folderName)));
-
-app.use('/api', api);
-
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-});
-
 
 export default app;
