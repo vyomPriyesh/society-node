1dddd93369c331462e4a572d1f6c33473f9aed09
vyom Priyesh <vyompriyesh@gmail.com>
2025-10-11 13:39:27 +0530

    main

--- Config.js
+++ Config.js
@@ -4,8 +4,8 @@ import "dotenv/config";
 const __filename = fileURLToPath(import.meta.url);
 export const __dirname = path.dirname(__filename);
 export const view = path.join(__dirname, 'Views');
-export const assetsUrl = path.join(__dirname, 'assets');
+export const assetsUrl = path.join(__dirname, 'images');
 export const baseUrl = process.env['BASE_URL']
-export const folderName = 'assets';
+export const folderName = 'images';
 
 
