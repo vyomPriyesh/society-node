92894876b0864fe0d7d811438d6b74774f23ac95
vyom Priyesh <vyompriyesh@gmail.com>
2025-10-11 13:28:26 +0530

    main

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 import express from "express";
 import cors from 'cors'
-// import connectDB from "./db/Connectdb.js";
+import connectDB from "./db/Connectdb.js";
 import api from "./routes/Api.js";
-// import { assetsUrl, folderName } from "./Config.js";
+import { assetsUrl, folderName } from "./Config.js";
 import path from "path";
 
 const app = express();
 const port = process.env.PORT
 const DATABASE_URL = process.env.DATABASE_URL
 
-// connectDB(DATABASE_URL);
+connectDB(DATABASE_URL);
 
-// app.use(express.json());
-// app.use(cors());
+app.use(express.json());
+app.use(cors());
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
@@ -22,13 +22,11 @@ app.use((err, req, res, next) => {
   });
 });
 app.get('/', (req, res) => res.send('Hello from Express on Vercel!'));
-// app.use(`/${folderName}`, express.static(assetsUrl));
-// app.use(express.static(path.resolve(assetsUrl, './', folderName)));
+app.use(`/${folderName}`, express.static(assetsUrl));
+app.use(express.static(path.resolve(assetsUrl, './', folderName)));
 
 app.use('/api', api);
 
-// app.listen(port, () => {
-//     console.log(`Example app listening at http://localhost:${port}`);
-// });
-
-export default app
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
