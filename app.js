f7fe269f7d01c6eb91cb0ca55465fde84da20654
vyom Priyesh <vyompriyesh@gmail.com>
2025-10-11 13:21:00 +0530

    main

--- app.js
+++ app.js
@@ -1,32 +1,4 @@
-import express from "express";
-import cors from 'cors'
-import connectDB from "./db/Connectdb.js";
-import api from "./routes/Api.js";
-import { assetsUrl, folderName } from "./Config.js";
-import path from "path";
-
+import express from 'express';
 const app = express();
-const port = process.env.PORT
-const DATABASE_URL = process.env.DATABASE_URL
-
-connectDB(DATABASE_URL);
-
-app.use(express.json());
-app.use(cors());
-
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({
-    success: false,
-    error: "Internal server error",
-  });
-});
-app.get('/', (req, res) => res.send('Hello from Express on Vercel!'));
-app.use(`/${folderName}`, express.static(assetsUrl));
-app.use(express.static(path.resolve(assetsUrl, './', folderName)));
-
-app.use('/api', api);
-
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-});
+app.get('/', (req, res) => res.send('Hello Vercel'));
+export default app;
